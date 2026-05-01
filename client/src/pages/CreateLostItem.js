bfb9206eb62c5b2435342c530a4aff2273d8a3b8
Thehawk2006 <tristenhawkins23@gmail.com>
2026-04-30 21:24:29 -0400

    Fix heading format in CreateLostItem component

--- client/src/pages/CreateLostItem.js
+++ client/src/pages/CreateLostItem.js
@@ -48,7 +48,7 @@ function CreateLostItem() {
 
   return (
     <div className="card">
-      <h2>Report a Lost Item</h2>
+      <h2>Report a Lost Item:</h2>
       <input name="user_id" placeholder="Your user ID" onChange={handleChange} />
       <select name="name" onChange={handleChange} defaultValue="">
         <option value="" disabled>Select item type</option>
